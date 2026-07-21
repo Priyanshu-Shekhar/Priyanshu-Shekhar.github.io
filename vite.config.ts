531af7a415089579b0964101bfc3da6e9a48b570
Priyanshu Shekhar <114723518+Priyanshu-Shekhar@users.noreply.github.com>
2026-07-20 22:37:12 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-
-  base: "https://github.com/Priyanshu-Shekhar/Priyanshu-Shekhar.github.io.git",
+  plugins: [react(), tailwindcss()],
 });
